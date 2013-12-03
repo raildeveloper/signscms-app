39e07572029307922eeb6b8ba376b3317823a32b
Paritosh Nandwani <paritosh.nandwani@transport.nsw.gov.au>
2013-12-04 10:48:23 +1100

    Test Cases updated.

--- gtfs-web/src/test/java/au/gov/nsw/railcorp/gtfs/servlet/ProtoListenerServletTest.java
+++ gtfs-web/src/test/java/au/gov/nsw/railcorp/gtfs/servlet/ProtoListenerServletTest.java
@@ -1,4 +1,5 @@
 // RailCorp 2012
+
 package au.gov.nsw.railcorp.gtfs.servlet;
 
 import static org.mockito.Mockito.mock;
@@ -6,14 +7,22 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import au.gov.nsw.railcorp.gtfs.converter.GeneralProtocolBufferConverter;
+import au.gov.nsw.railcorp.gtfs.converter.TripUpdateConverter;
+import au.gov.nsw.transport.rtta.intf.tripmodel.pb.generated.Tripmodel.TripListMessage;
+import au.gov.nsw.transport.rtta.intf.tripmodel.pb.generated.Tripmodel.TripModelEntityMessage;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import junit.framework.TestCase;
 
 import org.junit.After;
 import org.junit.Before;
@@ -22,19 +31,18 @@ import org.mockito.InjectMocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import junit.framework.TestCase;
-
-
 /**
- * @author John
- *
+ * @author Paritosh
  */
+
 public class ProtoListenerServletTest extends TestCase {
 
-    private static final Logger log = LoggerFactory.getLogger(CSVListenerServletTest.class);
+    private static final Logger log = LoggerFactory
+    .getLogger(CSVListenerServletTest.class);
+
     @InjectMocks
     ProtoListenerServlet protoServlet;
-    
+
     /**
      * {@inheritDoc}
      * @see junit.framework.TestCase#setUp()
@@ -59,58 +67,95 @@ public class ProtoListenerServletTest extends TestCase {
         protoServlet = null;
     }
 
-    // Servlet Test Cases:
-    // pass CSV in request, verify recieved by converter
-    // verify 'failed conversion' returns failed message
-    // verify successful conversion returns success message
-    
-    /**
-     * Test method for {@link au.gov.nsw.railcorp.gtfs.servlet.CsvListenerServlet#handleRequest(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
-     * @throws IOException 
-     * @throws ServletException 
-     */
     @Test
-    public void testHandleRequestNormal() throws ServletException, IOException {
+    public void testHandleRequestTripUpdateMessage() throws IOException,
+    ServletException {
 
-        log.info("testHandleRequestNormal");
-        final String inputString = "Testing CSV Content";
-        final ServletInputStream in = mock(ServletInputStream.class); //new ByteArrayInputStream(inputString.getBytes());
-        when(protoServlet.getConverter().storeProtocolBuffer(in)).thenReturn(true);
+        log.info("testHandleRequestTripUpdateMessage");
+
+        // Set up the ProtoBuff Input Stream
+
+        TripListMessage.Builder builder = TripListMessage.newBuilder();
+        builder.setMsgTimestamp(System.currentTimeMillis() / 1000L);
+        // TripListMessage tripListMessage = builder.build();
+
+        final TripModelEntityMessage.Builder tripModelEntityMessage = TripModelEntityMessage.newBuilder();
+
+        tripModelEntityMessage.setTimeStamp(System.currentTimeMillis() / 1000L);
+        tripModelEntityMessage.setActiveTrips(builder);
+        TripModelEntityMessage message = tripModelEntityMessage.build();
+        final byte[] messageByte = message.toByteArray();
+        System.out.println("message " + message.toString());
+        final InputStream byteIn = new ByteArrayInputStream(messageByte);
+
+        final TripUpdateConverter converter = mock(TripUpdateConverter.class);
+        protoServlet.setConverter(converter);
+
         final HttpServletRequest request = mock(HttpServletRequest.class);
         final HttpServletResponse response = mock(HttpServletResponse.class);
+
         final ServletContext sContext = mock(ServletContext.class);
-        final PrintWriter print = mock(PrintWriter.class);
+        final PrintWriter writer = mock(PrintWriter.class);
         when(request.getServletContext()).thenReturn(sContext);
-        when(request.getContentLength()).thenReturn(inputString.length());
-        when(request.getInputStream()).thenReturn(in);
-        when(sContext.getServletContextName()).thenReturn("TestCSVListener");
-        when(response.getWriter()).thenReturn(print);
-        
+        when(request.getInputStream()).thenReturn(new ServletInputStream() {
+
+            @Override
+            public int read() throws IOException {
+
+                return byteIn.read();
+            }
+        });
+        when(response.getWriter()).thenReturn(writer);
+        when(sContext.getServletContextName()).thenReturn("testHandleRequestTripUpdateMessage");
+
+        // when(TripModelEntityMessage.parseFrom(inputStream)).thenReturn(message);
+        when(protoServlet.getConverter().processLoadTripUpdates(message)).thenReturn(true);
+
         protoServlet.handleRequest(request, response);
-        verify(protoServlet.getConverter()).storeProtocolBuffer(in);
-        verify(print).append("Successful Update");
+        verify(protoServlet.getConverter()).processLoadTripUpdates(message);
+        verify(writer).append("Successful Update");
     }
 
     @Test
-    public void testHandleRequestFailedConvert() throws IOException, ServletException {
+    public void testHandleEmptyMessage() throws IOException, ServletException {
 
-        log.info("testHandleRequestFailedConvert");
-        final String inputString = "Testing Failed CSV Content";
-        final ServletInputStream in = mock(ServletInputStream.class); //new ByteArrayInputStream(inputString.getBytes());
-        when(protoServlet.getConverter().storeProtocolBuffer(in)).thenReturn(false);
+        log.info("testHandleEmptyMessage");
+
+        final TripUpdateConverter converter = mock(TripUpdateConverter.class);
+        protoServlet.setConverter(converter);
+
         final HttpServletRequest request = mock(HttpServletRequest.class);
         final HttpServletResponse response = mock(HttpServletResponse.class);
+
         final ServletContext sContext = mock(ServletContext.class);
-        final PrintWriter print = mock(PrintWriter.class);
+        final PrintWriter writer = mock(PrintWriter.class);
+
+        final TripModelEntityMessage.Builder tripModelEntityMessage = TripModelEntityMessage.newBuilder();
+
+        tripModelEntityMessage.setTimeStamp(System.currentTimeMillis() / 1000L);
+
+        TripModelEntityMessage message = tripModelEntityMessage.build();
+        final byte[] messageByte = message.toByteArray();
+        System.out.println("message " + message.toString());
+        final InputStream byteIn = new ByteArrayInputStream(messageByte);
+
         when(request.getServletContext()).thenReturn(sContext);
-        when(request.getContentLength()).thenReturn(inputString.length());
-        when(request.getInputStream()).thenReturn(in);
-        when(sContext.getServletContextName()).thenReturn("TestCSVListener");
-        when(response.getWriter()).thenReturn(print);
-        
+        when(request.getInputStream()).thenReturn(new ServletInputStream() {
+
+            @Override
+            public int read() throws IOException {
+
+                return byteIn.read();
+            }
+        });
+        when(response.getWriter()).thenReturn(writer);
+        when(sContext.getServletContextName()).thenReturn("testHandleEmptyMessage");
+
+        when(protoServlet.getConverter().processLoadTripUpdates(message)).thenReturn(false);
         protoServlet.handleRequest(request, response);
-        verify(protoServlet.getConverter()).storeProtocolBuffer(in);
-        verify(print).append("Failed Protocol Buffer validation processing");
+        verify(protoServlet.getConverter()).processLoadTripUpdates(message);
+        verify(writer).append("Failed Protocol Buffer validation processing");
+
     }
-    
+
 }
