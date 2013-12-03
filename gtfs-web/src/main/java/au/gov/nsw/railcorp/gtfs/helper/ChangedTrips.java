af4f3816d01400517573baa96de3d640b3ba1035
Paritosh Nandwani <paritosh.nandwani@transport.nsw.gov.au>
2013-12-04 10:46:51 +1100

    Object to contain TripUpdate Changed Trips

---
+++ gtfs-web/src/main/java/au/gov/nsw/railcorp/gtfs/helper/ChangedTrips.java
@@ -0,0 +1,41 @@
+// RailCorp 2013
+
+package au.gov.nsw.railcorp.gtfs.helper;
+
+import au.gov.nsw.railcorp.gtfs.model.Trip;
+
+import java.util.List;
+
+/**
+ * Changed Trips.
+ * @author paritosh
+ */
+public class ChangedTrips {
+
+    private static ChangedTrips sINSTANCE;
+
+    private List<Trip> changedTrips;
+
+    /**
+     * Changed Trips.
+     * @return ChangedTrips
+     */
+    public static synchronized ChangedTrips getInstance() {
+
+        if (sINSTANCE == null) {
+            sINSTANCE = new ChangedTrips();
+
+        }
+        return sINSTANCE;
+    }
+
+    public List<Trip> getChangedTrips() {
+
+        return changedTrips;
+    }
+
+    public void setChangedTrips(List<Trip> changedtrips) {
+
+        this.changedTrips = changedtrips;
+    }
+}
